6416c58229a40b1eb08fb6e722ba7579968d46f2
justynasobczak <justyna.sobczak@gmail.com>
2019-05-27 17:58:51 +1200

    Added: tests for games

--- PlayBoardGames.Tests/GameEFTests.cs
+++ PlayBoardGames.Tests/GameEFTests.cs
@@ -11,38 +11,110 @@ namespace PlayBoardGames.Tests
         [Fact]
         public void Can_Add_Game()
         {
-            // In-memory database only exists while the connection is open
-            var connection = new SqliteConnection("DataSource=:memory:");
-            connection.Open();
+            //Arrange
+            var connection = TestBase.OpenConnection();
 
             try
             {
-                var options = new DbContextOptionsBuilder<ApplicationDBContext>()
-                    .UseSqlite(connection)
-                    .Options;
+                var options = TestBase.CreateDbContextOptions(connection);
 
                 // Create the schema in the database
                 using (var context = new ApplicationDBContext(options))
                 {
                     context.Database.EnsureCreated();
                 }
-
+                
+                //Act
                 // Run the test against one instance of the context
                 using (var context = new ApplicationDBContext(options))
-                {   
+                {
                     var gameRepository = new EFGameRepository(context);
-                    gameRepository.SaveGame(new Game { Title = "Test"});
+                    gameRepository.SaveGame(new Game {Title = "TestToAdd"});
                 }
 
+                //Assert
                 // Use a separate instance of the context to verify correct data was saved to database
                 using (var context = new ApplicationDBContext(options))
                 {
                     Assert.Equal(1, context.Games.Count());
-                    Assert.Equal("Test", context.Games.Single().Title);
+                    Assert.Equal("TestToAdd", context.Games.Single().Title);
                 }
             }
             finally
+            {
+                connection.Close();
+            }
+        }
 
+        [Fact]
+        public void Can_Edit_Game()
+        {
+            //Arrange
+            var connection = TestBase.OpenConnection();
+
+            try
+            {
+                var options = TestBase.CreateDbContextOptions(connection);
+
+                using (var context = new ApplicationDBContext(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Games.Add(new Game {GameID = 1, Title = "TestToModify"});
+                    context.SaveChanges();
+                }
+
+                //Act
+                using (var context = new ApplicationDBContext(options))
+                {
+                    var gameRepository = new EFGameRepository(context);
+                    gameRepository.SaveGame(new Game {GameID = 1, Title = "TestAfterModification"});
+                }
+
+                //Assert
+                using (var context = new ApplicationDBContext(options))
+                {
+                    Assert.Equal(1, context.Games.Count());
+                    Assert.Equal("TestAfterModification", context.Games.Single().Title);
+                    Assert.Equal(1, context.Games.Single().GameID);
+                }
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+        
+        [Fact]
+        public void Can_Delete_Game()
+        {
+            //Arrange
+            var connection = TestBase.OpenConnection();
+
+            try
+            {
+                var options = TestBase.CreateDbContextOptions(connection);
+
+                using (var context = new ApplicationDBContext(options))
+                {
+                    context.Database.EnsureCreated();
+                    context.Games.Add(new Game {GameID = 1, Title = "TestToDelete"});
+                    context.SaveChanges();
+                }
+
+                //Act
+                using (var context = new ApplicationDBContext(options))
+                {
+                    var gameRepository = new EFGameRepository(context);
+                    gameRepository.DeleteGame(1);
+                }
+
+                //Assert
+                using (var context = new ApplicationDBContext(options))
+                {
+                    Assert.Equal(0, context.Games.Count());
+                }
+            }
+            finally
             {
                 connection.Close();
             }
